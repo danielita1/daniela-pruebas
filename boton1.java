d247268ea83d425cf866822dc7e18a9c5cb96bdd
danielita1 <128556279+danielita1@users.noreply.github.com>
2023-04-21 14:35:22 -0400

    Actualizar mostrar nombre

--- boton1.java
+++ boton1.java
@@ -1,9 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+import java. lang.reflect.field;
 
-public class boton1
 {
-   
- }
+   public STring sdaniela= new String("daniela");
+   publico STring sdaniela=new String("daniela");
+
+public static void main (STring[] args) 
+throws java.lang.Exception,java.lang.IllegalArgumentException,lang.IllegalAccessException
+{
+ Ideone t = new Ideone()
+ for(Field f: t.getClass().getFields()){
+System.out.println("nombre:"+ f.getName());
+   }
+  }
+}
+
+
+ 
 
     
     
